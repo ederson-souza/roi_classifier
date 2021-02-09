525797c183868f7bf602ff3cb87becfad7efa3e4
edinhoadm <ederson.andre@me.com>
2021-02-09 10:11:01 -0300

    Real time implementation and minor fixes

--- roi_classifier.py
+++ roi_classifier.py
@@ -13,6 +13,13 @@ from gluoncv.model_zoo import get_model
 from bounding_box import bounding_box as bb
 
 
+def finish():
+  cap.release()
+  cv2.destroyAllWindows()
+  video.release()
+  os.remove("crop.jpg")
+
+
 if __name__ == '__main__':
 
   # Current Directory
@@ -33,7 +40,7 @@ if __name__ == '__main__':
   total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
 
   # Set starting frame
-  num_frame = 360
+  num_frame = 0
 
   # Model Configuration
   transform_fn = transforms.Compose([
@@ -63,72 +70,75 @@ if __name__ == '__main__':
     # Video settings
   video = cv2.VideoWriter(OUTPUT_PATH, cv2.VideoWriter_fourcc(*'mp4v'), 25, (int(width),int(height)),True)
   
-  num_start_frame = num_frame
+  num_start_frame = 360
   
   # Start Classification 
   first = True
 
   while(cap.isOpened()):
     
-    if num_frame > total_frames:
-      break
-    
-    try:
-      cap.set(1, num_frame)  
-      _ , frame = cap.read() #read frame
+    ret, frame = cap.read() #read frame
 
-      if first: # if first frame, open Roi Selector
-        r = cv2.selectROI(frame)
+    if ret:     
+
+      if num_frame >= num_start_frame:
         
-        # get coordinates
-        roi_x1 = int(r[0])
-        roi_x2 = int(r[0] + r[2])
-        roi_y1 = int(r[1])
-        roi_y2 = int(r[1]+r[3])
-        cv2.destroyAllWindows()
-        first = False
-      
-      # Print progress 
-      print(f'{(num_frame-num_start_frame)}/{int(total_frames-num_start_frame)} - {((num_frame-num_start_frame)/(total_frames-num_start_frame)*100):.2f}%', end='\r')
-      
-      # Crop Roi
-      crop_frame = frame[roi_y1:roi_y2, roi_x1:roi_x2]
-      
-      # Save a temporary file
-      cv2.imwrite('crop.jpg', crop_frame)
-      
-      # Read and transform the image
-      crop_frame = image.imread('crop.jpg')
-      crop_frame = transform_fn(crop_frame)
+        if first: # if first frame, open Roi Selector
+          r = cv2.selectROI(frame)
+          
+          # get coordinates
+          roi_x1 = int(r[0])
+          roi_x2 = int(r[0] + r[2])
+          roi_y1 = int(r[1])
+          roi_y2 = int(r[1]+r[3])
+          cv2.destroyAllWindows()
+          first = False
+        
+        # Print progress 
+        # print(f'{(num_frame-num_start_frame)}/{int(total_frames-num_start_frame)} - {((num_frame-num_start_frame)/(total_frames-num_start_frame)*100):.2f}%', end='\r')
+        
+        # Crop Roi
+        crop_frame = frame[roi_y1:roi_y2, roi_x1:roi_x2]
+        
+        # Save a temporary file
+        cv2.imwrite('crop.jpg', crop_frame)
+        
+        # Read and transform the image
+        crop_frame = image.imread('crop.jpg')
+        crop_frame = transform_fn(crop_frame)
 
-      # Make Prediction
-      output = net(crop_frame.expand_dims(axis=0))
-      
-      # Get the most probable class index
-      index = nd.argmax(output, axis=1).astype('int')
-      
-      # Get the class's probability and convert to a formatted string
-      prob = f'{nd.softmax(output)[0][index].asscalar()*100:.2f}%'
+        # Make Prediction
+        output = net(crop_frame.expand_dims(axis=0))
+        
+        # Get the most probable class index
+        index = nd.argmax(output, axis=1).astype('int')
+        
+        # Get the class's probability and convert to a formatted string
+        prob = f'{nd.softmax(output)[0][index].asscalar()*100:.2f}%'
 
-      # Check whether the most probable class is automobile
-      if class_names[index.asscalar()] == 'automobile':
-        class_ = 'car'
+        # Check whether the most probable class is automobile
+        if class_names[index.asscalar()] == 'automobile':
+          class_ = 'car'
+        else:
+          class_ = 'not a car'
+
+
+      # Draw ROI and prediction label
+        bb.add(frame, roi_x1, roi_y1, roi_x2, roi_y2, class_ + '-' + prob, 'yellow')
+
+      # Record the output frame
+        video.write(frame)
+
+      # Go to the next frame
+        num_frame += 1  
+
+        cv2.imshow('Result', frame)
+        if cv2.waitKey(1) == 27:
+          finish()
+
       else:
-        class_ = 'not a car'
+        num_frame += 1
+        continue
 
-    except:
-      break
-    
-  # Draw ROI and prediction label
-    bb.add(frame, roi_x1, roi_y1, roi_x2, roi_y2, class_ + '-' + prob, 'yellow')
-
-  # Record the output frame
-    video.write(frame)
-
-  # Go to the next frame
-    num_frame += 1  
-   
-
-  cap.release()
-  video.release()
-  os.remove("crop.jpg")
+    else:
+      finish()
